f11de61f3abda19f09ae4ae83cbf62cb13c4d6bf
zuberol <zuber.kuba@gmail.com>
2020-06-11 13:01:29 +0200

    first look

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('submit-btn').addEventListener('click', (event) => {
+    const text = document.getElementById("task-generator__span").innerHTML;
+    const newTask = document.createElement("li");
+    newTask.innerHTML = text;
+    document.getElementById('tasks__listID').appendChild(newTask);
+})
+
+
+
+const l = document.getElementById('leki');
+l.addEventListener('click', function(){
+    console.log('clicked');
+    // rm
+    
+});
