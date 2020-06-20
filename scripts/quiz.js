29334b43a46ebc3524918ece01693a328115cbed
zuberol <50877692+zuberol@users.noreply.github.com>
2020-06-20 14:44:20 +0200

    quiz added

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -8,8 +8,46 @@ class Flashcard {
     }
 }
 
-let f = new Flashcard("A srasz?", "a ty?", "aco.png", "e sralem xd");
+let f1 = new Flashcard("A srasz?", "a ty?", "https://www.euractiv.pl/wp-content/uploads/sites/6/2020/05/max-baskakov-OzAeZPNsLXk-unsplash-scaled-e1588503189401-800x450.jpg", "e sralem xd");
+let f2 = new Flashcard("A sraszdsadsadasd?", "a tyyyyyy?", "accccccco.png", "eeeeeeeeeeeeee sralem xd");
+let f3 = new Flashcard("A dsasdadsa?", "tytytytyty?", "cocoococooco", "eeeeeeeeeeeeee ??SDA?!?!");
 
-console.log(f);
+// console.log(f1);
 
-const data = {};
+const database = [f1, f2, f3];
+
+
+// function previous(){
+
+
+// }
+
+function revealAnswer(){
+    const ans = document.getElementById('answer');
+    ans.style.display = 'block';
+}
+function hideAnswer(){
+    const ans = document.getElementById('answer');
+    ans.style.display = 'none';
+}
+
+
+function next(){
+    const ans = document.getElementById('answer');
+    const question = document.getElementById('question');
+    const img = document.querySelector('.flashcard-wrapper__image img');
+
+    const fc = database.pop();
+
+
+    img.src = fc.picture;
+    
+
+    ans.innerText = fc.answer;
+    question.innerText = fc.question;
+
+    hideAnswer();
+
+}
+
+
