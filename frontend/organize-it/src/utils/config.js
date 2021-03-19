ee9ecf65a3e6aadfeae4c47ace401556568adb99
zuberol <zuber.kuba@gmail.com>
2021-03-19 19:43:43 +0100

    cfg added

---
+++ frontend/organize-it/src/utils/config.js
@@ -0,0 +1,15 @@
+const CODE_DIRS = [
+    "/Users/zuber/js/organize-it/receivedFiles/zeauberg/code-samples/java",
+    "/Users/zuber/js/ref/react-scripts"
+];
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_PATH = 'token.json';
+
+const BACKEND_BASE_URL = "http://localhost:8080";
+
+module.exports = {
+    CODE_DIRS: CODE_DIRS,
+    SCOPES: SCOPES,
+    TOKEN_PATH: TOKEN_PATH,
+    BACKEND_BASE_URL: BACKEND_BASE_URL
+}
